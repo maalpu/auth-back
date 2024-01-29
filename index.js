17e5861f672301fd1e613c1153345ac4d9f02593
Mario <mariopuebla@gmail.com>
2024-01-28 22:15:34 -0300

    Habilitando IPv6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import app from './app.js'
 import { PORT } from './src/config.js'
 
-app.listen(PORT)
-console.log('Server runing on port ' + PORT)
+app.listen(PORT, '::', () => {
+  console.log(`Server listening on [::]${port}`);
+})
